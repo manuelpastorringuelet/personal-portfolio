c3475d664444768fab31371d06d43c303b38c3f8
Manuel Pastor <pastorringuelet@gmail.com>
2023-08-30 17:53:04 +0200

    added projects

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import profile from "@/schemas/profile";
 import job from "@/schemas/job";
+import project from "@/schemas/project";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, job],
+  types: [profile, job, project],
 };
