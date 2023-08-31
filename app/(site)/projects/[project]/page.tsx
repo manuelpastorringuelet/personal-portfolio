59ebb6d14b4dcf6e4d9bca36845a96fd0ace80a9
Manuel Pastor <pastorringuelet@gmail.com>
2023-08-31 22:46:11 +0200

    fixed route image

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import { getSingleProject } from "@/sanity/sanity.query";
 import type { ProjectType } from "@/types";
 import { PortableText } from "@portabletext/react";
-import fallBackImage from "@/public/project.png";
+import fallBackImage from "@/public/images/project.png";
 
 type Props = {
   params: {
